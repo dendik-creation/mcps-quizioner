99444798dcbb1b79a79876e6f0c0b486989f0c9d
Syahrul Imam Said <syahrulimamsaid@gmail.com>
2025-08-18 18:11:43 +0700

    Re-Push Migrations, Model, and Controller

---
+++ database/migrations/2025_08_16_200824_create_participant_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('participant', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname',50);
+            $table->string('nisn',10);
+            $table->foreignId('school_id');
+            $table->string('class',10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('participant');
+    }
+};
