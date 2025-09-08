52a26099395c22269bbe41aa31a13a60b4b85f4b
dendik-creation <setiawandendik0205@gmail.com>
2025-09-08 19:55:28 +0700

    [fix] remove unused console.log

--- resources/js/pages/Questionnaires/Demo.tsx
+++ resources/js/pages/Questionnaires/Demo.tsx
@@ -97,7 +97,6 @@ export default function Demo({ app_name }: { app_name: string }) {
     const handleEssayChange = (content: string) => {
         const updated = [...essayAnswers];
         updated[currentQuestion] = content;
-        console.log(updated);
         setEssayAnswers(updated);
     };
 
